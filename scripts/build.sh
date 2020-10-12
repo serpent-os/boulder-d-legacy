3b954acdbdd33927fc3b60b2779e87d4473c69af
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-10-12 11:03:03 +0100

    Memory loss is a bitch
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,7 +18,7 @@ function buildProject()
 if [[ "$MODE" == "release" ]]; then
     COMPILER="ldc2"
     buildProject
-    strip bin/moss
+    strip bin/boulder
 elif [[ "$MODE" == "optimized" ]]; then
     MODE="release"
     buildProject
