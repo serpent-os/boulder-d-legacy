759722a35def51faabedfb766997a41ccf127132
Ikey Doherty <ikey@serpentos.com>
2021-02-10 21:24:38 +0000

    build/emitter: Stop using the old defunct APIs
    We're killing the crappy APIs as they're memory abuse nightmare and
    have no clear interface pattern/contract. For now we'll continue to
    emit empty packages with nothing but an ArchiveHeader present to
    describe the package as a valid moss package, but that is all.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/boulder/build/emitter.d
+++ source/boulder/build/emitter.d
@@ -28,14 +28,6 @@ import boulder.build.collector : BuildCollector;
 import moss.format.source.package_definition;
 import moss.format.source.source_definition;
 import moss.format.binary.writer;
-import moss.format.binary.payload;
-import moss.format.binary.content_payload;
-import moss.format.binary.index;
-import moss.format.binary.index_payload;
-import moss.format.binary.meta_payload;
-import moss.format.binary.layout;
-import moss.format.binary.layout_payload;
-import moss.format.binary.record;
 
 /**
  * Resulting Package is only buildable once it contains
@@ -131,100 +123,6 @@ private:
         }
 
         writefln("Creating package %s...", finalPath);
-
-        /* Encode metapayload */
-        auto meta = MetaPayload();
-        meta.compression = PayloadCompression.Zstd;
-
-        /* Add relevant entries */
-        meta.addRecord(RecordTag.Name, pkg.pd.name);
-        meta.addRecord(RecordTag.Version, pkg.source.versionIdentifier);
-        meta.addRecord(RecordTag.Release, pkg.source.release);
-        meta.addRecord(RecordTag.Summary, pkg.pd.summary);
-        meta.addRecord(RecordTag.Description, pkg.pd.description);
-        meta.addRecord(RecordTag.Homepage, pkg.source.homepage);
-        writer.addPayload(cast(Payload*)&meta);
-
-        /* Add content payload */
-        auto content = ContentPayload();
-        content.compression = PayloadCompression.Zstd;
-
-        auto indexes = IndexPayload();
-        indexes.compression = PayloadCompression.Zstd;
-
-        import std.file : getSize;
-
-        ulong startOffset = 0;
-
-        /* Encode content in deduplicated fashion */
-        foreach (ref origin; dupeSet)
-        {
-            auto hash = origin.hash;
-            auto source = origin.originPath;
-            if (content.hasFile(hash))
-            {
-                continue;
-            }
-
-            auto size = source.getSize();
-            const auto endOffset = startOffset + size;
-            content.addFile(hash, source);
-            auto idx = IndexEntry();
-            idx.size = size;
-            idx.start = startOffset;
-            idx.end = endOffset;
-            indexes.addEntry(idx, hash);
-            startOffset = endOffset;
-        }
-
-        /* Apply layout to disk */
-        auto layouts = LayoutPayload();
-        layouts.compression = PayloadCompression.Zstd;
-
-        /**
-         * Convert a FileAnalysis into a LayoutEntry struct
-         */
-        LayoutEntry fromAnalysis(ref FileAnalysis fa)
-        {
-            LayoutEntry ret;
-            const auto st = fa.stat;
-            ret.uid = st.st_uid;
-            ret.gid = st.st_gid;
-            ret.mode = st.st_mode;
-
-            /* TODO: Set global timestamp */
-            ret.time = st.st_mtime;
-            ret.type = fa.type;
-
-            return ret;
-        }
-
-        /**
-         * Push a layout entry
-         */
-        void pushLayout(ref FileAnalysis fa)
-        {
-            auto le = fromAnalysis(fa);
-
-            switch (fa.type)
-            {
-            case FileType.Regular:
-            case FileType.Symlink:
-                layouts.addEntry(le, fa.data, fa.path);
-                break;
-            case FileType.Directory:
-                layouts.addEntry(le, fa.path);
-                break;
-            default:
-                assert(0, "Unsupported filetype");
-            }
-        }
-
-        fileSet.each!((ref fa) => pushLayout(fa));
-
-        writer.addPayload(cast(Payload*)&indexes);
-        writer.addPayload(cast(Payload*)&content);
-        writer.addPayload(cast(Payload*)&layouts);
 
         writer.flush();
     }
