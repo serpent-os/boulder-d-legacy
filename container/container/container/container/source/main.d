2c59e91d1433f3519d9ab0e9782ad1b11cf7ca2f
Ikey Doherty <ikey@serpentos.com>
2022-01-20 03:05:01 +0000

    Restructure main entry to use getopt and not be hardcoded mess
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- container/container/container/container/source/main.d
+++ container/container/container/container/source/main.d
@@ -28,6 +28,11 @@ import std.path : buildPath;
 import moss.container.context;
 import core.sys.posix.sys.stat : umask;
 import std.conv : octal;
+import std.getopt;
+import std.file : exists, isDir;
+import std.string : empty;
+import std.stdio : stderr;
+import core.sys.posix.unistd : geteuid;
 
 /**
  * Main entry point into moss-container
@@ -35,16 +40,56 @@ import std.conv : octal;
 int main(string[] args)
 {
     umask(octal!22);
+    string rootfsDir = null;
+    bool fakeroot = false;
+    bool networking = false;
 
-    /* Establish working rules */
-    context.rootfs = "/home/ikey/serpent/moss/destdir";
-    context.fakeroot = true;
+    auto opts = getopt(args, std.getopt.config.bundling, "directory|d",
+            &rootfsDir, "fakeroot|f", &fakeroot, "networking|n", &networking);
+
+    if (opts.helpWanted)
+    {
+        defaultGetoptPrinter("Usage: ", opts.options);
+        return 0;
+    }
+
+    /* Ensure rootfs directory set */
+    if (rootfsDir.empty)
+    {
+        stderr.writeln("You must set a directory with the -d option");
+        return 1;
+    }
+
+    if (!rootfsDir.exists || !rootfsDir.isDir)
+    {
+        stderr.writefln("The directory specified does not exist: %s", rootfsDir);
+    }
+
+    /* Set the rootfs */
+    context.rootfs = rootfsDir;
+
+    /* Ensure we're running as root */
+    if (geteuid() != 0)
+    {
+        stderr.writefln("%s: Needs to run as root", args[0]);
+        return 1;
+    }
+
+    string[] commandLine = args.length > 0 ? args[1 .. $] : null;
+    if (commandLine.length < 1)
+    {
+        commandLine = ["/bin/bash", "--login"];
+    }
+
+    string programName = commandLine[0];
+    string[] programArgs = commandLine[0].length > 0 ? commandLine[1 .. $] : null;
+
+    context.fakeroot = fakeroot;
     context.workDir = "/";
     context.environment["PATH"] = "/usr/bin:/bin";
 
     auto c = new Container();
-    c.networking = false;
-    c.add(Process("/bin/bash", ["--login"]));
-
+    c.networking = networking;
+    c.add(Process(programName, programArgs));
     return c.run();
 }
