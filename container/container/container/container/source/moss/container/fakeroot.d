4e4a61940e99275f43e2c145ddadfc427892c735
Ikey Doherty <ikey@serpentos.com>
2022-01-24 19:05:18 +0000

    Redo all module headers in code-style template
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- container/container/container/container/source/moss/container/fakeroot.d
+++ container/container/container/container/source/moss/container/fakeroot.d
@@ -1,30 +1,18 @@
-/*
- * This file is part of moss-container.
- *
- * Copyright © 2020-2022 Serpent OS Developers
- *
- * This software is provided 'as-is', without any express or implied
- * warranty. In no event will the authors be held liable for any damages
- * arising from the use of this software.
- *
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- *
- * 1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software. If you use this software
- *    in a product, an acknowledgment in the product documentation would be
- *    appreciated but is not required.
- * 2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any source distribution.
- */
+/* SPDX-License-Identifier: Zlib */
 
 /**
- * Disclaimer: We fully intend to replace fakeroot with something more efficient
- * in future but for now let's just manage that stuff here.
+ * Fakeroot discovery
+ *
+ * Basic discovery of the `fakeroot` binary to ensure we always
+ * prefer the `fakeroot-sysv` variant over the `fakeroot-tcp`
+ * variant which is painfully slow on Linux.
+ *
+ * Notes: We intend to replace fakeroot usage in future with
+ * our own library.
+ *
+ * Authors: © 2020-2022 Serpent OS Developers
+ * License: ZLib
  */
-
 module moss.container.fakeroot;
 
 import moss.container.context;
