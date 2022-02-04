97fac477bfe49c20ad67b1d1d2fae666e639757d
Ikey Doherty <ikey@serpentos.com>
2022-02-04 01:51:16 +0000

    Start the new chef module
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/chef/package.d
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: Zlib */
+
+/**
+ * Chef - Recipe Manipulation
+ *
+ * Generation and manipulation of source recipe files that can then be consumed
+ * by boulder.
+ *
+ * Authors: © 2020-2022 Serpent OS Developers
+ * License: ZLib
+ */
+
+module chef;
