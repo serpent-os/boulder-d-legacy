0d6a3a45811e0cb778660fe3d70427191fc6c911
Ikey Doherty <ikey@serpentos.com>
2022-02-12 12:38:53 +0000

    boulder: Begin new stages submodule
    Before we begin the mad descent back into nightmare procedural
    code, we'll add a new Stage API so that we can split the new
    implementation across multiple files, simplifying development
    and ensuring each feature is given appropriate focus.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/boulder/stages/package.d
@@ -0,0 +1,81 @@
+/* SPDX-License-Identifier: Zlib */
+
+/**
+ * Boulder Stages
+ *
+ * Modular simplistic API for various stages in the execution of the
+ * boulder part of building, i.e. container and root encapsulation
+ *
+ * Authors: © 2020-2022 Serpent OS Developers
+ * License: ZLib
+ */
+
+module boulder.stages;
+
+public import boulder.buildjob : BuildJob;
+
+/** 
+ * The StageContext (i.e. BoulderController) exposes some useful properties
+ * to facilitate integration.
+ */
+public interface StageContext
+{
+    /**
+     * The current job
+     *
+     * Returns: Const reference to the BuildJob
+     */
+    pure @property BuildJob job() @safe @nogc nothrow const;
+
+    /**
+     * Path to moss
+     *
+     * Returns: Immutable string containing the path
+     */
+    pure @property immutable(string) mossBinary() @safe @nogc nothrow const;
+
+    /**
+     * Path to moss-container
+     *
+     * Returns: Immutable string containing the path
+     */
+    pure @property immutable(string) containerBinary() @safe @nogc nothrow const;
+}
+
+/**
+ * In order to facilitate flow control each stage execution must return
+ * their status
+ */
+public enum StageReturn
+{
+    /** Stage executed successfully */
+    Success,
+
+    /** Stage failed, bail from processing */
+    Failure,
+
+    /** Stage didn't need to run */
+    Skipped,
+}
+
+/**
+ * Each execution function for a stage must explicitly match our type
+ */
+public alias StageFunction = StageReturn function(scope StageContext context);
+
+/**
+ * A Stage is just an element to control the flow of execution in the
+ * boulder process. It is not the same as a mason build stage.
+ */
+public struct Stage
+{
+    /**
+     * Name for this execution stage
+     */
+    immutable(string) name;
+
+    /**
+     * The runnable method
+     */
+    immutable(StageFunction) functor;
+}
