f50169933132f76bd4913a039cf4ed109bdbd0f6
Ikey Doherty <ikey@serpentos.com>
2022-02-19 23:26:49 +0000

    Add skeleton for UpstreamCache
    The UpstreamCache will have specialist handling for
    retention of upstreams (sources) for build reuse.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/boulder/upstreamcache.d
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: Zlib */
+
+/**
+ * Upstream Cache
+ *
+ * Path mangling for retention of sources
+ *
+ * Authors: © 2020-2022 Serpent OS Developers
+ * License: ZLib
+ */
+
+module boulder.upstreamcache;
+
+/**
+ * The UpstreamCache provides persistent paths and
+ * deduplication facilities to permit retention of
+ * hash-indexed downloads across multiple builds.
+ */
+public final class UpstreamCache
+{
+
+}
