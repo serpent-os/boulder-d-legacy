b6d591158be113226bfb70ef31358faf602acef2
Ikey Doherty <ikey@serpentos.com>
2022-02-20 03:56:30 +0000

    stages/build_package: Add basic invocation *without* mason
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/boulder/stages/build_package.d
+++ source/boulder/stages/build_package.d
@@ -9,6 +9,10 @@
 
 module boulder.stages.build_package;
 
+import mason.build.util : executeCommand;
+import std.sumtype : match;
+import std.string : format;
+
 public import boulder.stages : Stage, StageReturn, StageContext;
 
 /**
@@ -21,5 +25,21 @@ public static immutable(Stage) stageBuildPackage = Stage("build-package", &build
  */
 static private StageReturn buildPackage(scope StageContext context)
 {
-    return StageReturn.Failure;
+    string[string] environ;
+    environ["PATH"] = "/usr/bin:/usr/sbin";
+    string[] args = [
+        "--directory", context.job.hostPaths.rootfs, "--bind-rw",
+        format!"%s=%s"(context.job.hostPaths.artefacts,
+                context.job.guestPaths.artefacts), "--bind-rw",
+        format!"%s=%s"(context.job.hostPaths.buildRoot,
+                context.job.guestPaths.buildRoot),
+        /*"--bind-rw", format!"%s=%s"(context.job.hostPaths.compilerCache, context.job.guestPaths.compilerCache),*/
+        "--bind-ro",
+        format!"%s=%s"(context.job.hostPaths.recipe, context.job.guestPaths.recipe),
+        "-s", "TERM=xterm-256color", "--fakeroot", "--", "bash", "--login",
+    ];
+    auto result = executeCommand(context.containerBinary, args, environ, "/");
+    auto ret = result.match!((int err) => err != 0 ? StageReturn.Failure
+            : StageReturn.Success, (e) => StageReturn.Failure);
+    return ret;
 }
