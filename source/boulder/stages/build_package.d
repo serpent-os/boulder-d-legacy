9811a5cae78af3a8346b4cdc8707c7f80f90bcc2
Ikey Doherty <ikey@serpentos.com>
2022-02-20 04:08:23 +0000

    stages/build_package: Mostly complete `mason` invocation
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/boulder/stages/build_package.d
+++ source/boulder/stages/build_package.d
@@ -10,6 +10,7 @@
 module boulder.stages.build_package;
 
 import mason.build.util : executeCommand;
+import std.path : buildPath;
 import std.sumtype : match;
 import std.string : format;
 
@@ -28,15 +29,28 @@ static private StageReturn buildPackage(scope StageContext context)
     string[string] environ;
     environ["PATH"] = "/usr/bin:/usr/sbin";
     string[] args = [
+        /* Root moss filesystem */
         "--directory", context.job.hostPaths.rootfs, "--bind-rw",
+        /* Output tree */
         format!"%s=%s"(context.job.hostPaths.artefacts,
                 context.job.guestPaths.artefacts), "--bind-rw",
+        /* Build root (with sources) */
         format!"%s=%s"(context.job.hostPaths.buildRoot,
                 context.job.guestPaths.buildRoot),
-        /*"--bind-rw", format!"%s=%s"(context.job.hostPaths.compilerCache, context.job.guestPaths.compilerCache),*/
+        /* ccache */
+        "--bind-rw",
+        format!"%s=%s"(context.job.hostPaths.compilerCache,
+                context.job.guestPaths.compilerCache),
+        /* recipe tree */
         "--bind-ro",
         format!"%s=%s"(context.job.hostPaths.recipe, context.job.guestPaths.recipe),
-        "-s", "TERM=xterm-256color", "--fakeroot", "--", "bash", "--login",
+        /* Enable colours */
+        "-s", "TERM=xterm-256color",
+        /* Fakeroot, end of options */
+        "--fakeroot", "--",
+        /* Real command to run */
+        "mason", "build", "-o", context.job.guestPaths.artefacts,
+        context.job.guestPaths.recipe.buildPath(context.job.name)
     ];
     auto result = executeCommand(context.containerBinary, args, environ, "/");
     auto ret = result.match!((int err) => err != 0 ? StageReturn.Failure
