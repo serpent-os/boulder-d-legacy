c5d0ab8dda726a5c8c2817c07c83b06e9301214a
Ikey Doherty <ikey@serpentos.com>
2022-02-20 00:44:52 +0000

    stages/fetch_upstreams: Add dumb handling of fetching
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/boulder/stages/fetch_upstreams.d
+++ source/boulder/stages/fetch_upstreams.d
@@ -14,14 +14,35 @@ module boulder.stages.fetch_upstreams;
 public import boulder.stages : Stage, StageReturn, StageContext;
 
 import std.algorithm : each;
+import moss.fetcher;
 
 /**
  * Grab all sources for build
  */
 public static immutable(Stage) stageFetchUpstreams = Stage("fetch-upstreams", &fetchUpstreams);
 
+/**
+ * Handle the actual fetching of upstreams
+ */
 static private StageReturn fetchUpstreams(scope StageContext context)
 {
     context.upstreamCache.constructDirs();
+    auto upstreams = context.job.recipe.upstreams.values;
+    foreach (u; upstreams)
+    {
+        /* No point downloading it again.. */
+        if (context.upstreamCache.contains(u))
+        {
+            continue;
+        }
+        auto spath = context.upstreamCache.stagingPath(u);
+        auto fetch = Fetchable(u.uri, spath, 0, FetchType.RegularFile, null);
+        context.fetcher.enqueue(fetch);
+    }
+    /* Run all fetches */
+    while (!context.fetcher.empty)
+    {
+        context.fetcher.fetch();
+    }
     return StageReturn.Failure;
 }
