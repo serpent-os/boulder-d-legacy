41ae1e8dcb148858d68768c78781e0bbb7e10440
Ikey Doherty <ikey@serpentos.com>
2022-06-14 20:56:34 +0100

    chef/metadata: Implement github support
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/chef/metadata/github.d
+++ source/chef/metadata/github.d
@@ -14,6 +14,7 @@ module chef.metadata.github;
 import std.typecons : Nullable;
 import moss.format.source.source_definition;
 import std.regex;
+import std.string : format;
 
 /**
  * Github automatically generated downloads
@@ -22,22 +23,50 @@ auto reGithubAutomatic = ctRegex!(
         r"\w+\:\/\/github\.com\/([A-Za-z0-9-_]+)\/([A-Za-z0-9-_]+)\/archive\/refs\/tags\/([A-Za-z0-9.-_]+)\.(tar|zip)");
 
 /**
+ * Mapping to our groups
+ */
+private static enum GithubIndex : int
+{
+    Owner = 1,
+    Project = 2,
+    Version = 3,
+}
+
+/**
  * Manually uploaded files on GitHub
  */
 auto reGithubManual = ctRegex!(
         r"\w+\:\/\/github\.com\/([A-Za-z0-9-_]+)\/([A-Za-z0-9-_]+)\/releases\/download\/([A-Za-z0-9-_.]+)\/.*");
 
 /**
- * More advanced matching.
+ * More advanced matching of GitHub specific URIs
  */
 public struct GithubMatcher
 {
     /**
-     * Not yet implemented
+     * Match full unmodified string with 2 regex patterns
      */
     Nullable!(SourceDefinition, SourceDefinition.init) match(in string uri)
     {
         Nullable!(SourceDefinition, SourceDefinition.init) ret = SourceDefinition.init;
+
+        auto matchers = [
+            uri.matchFirst(reGithubAutomatic), uri.matchFirst(reGithubManual),
+        ];
+
+        foreach (m; matchers)
+        {
+            if (m.empty)
+            {
+                continue;
+            }
+            auto sd = SourceDefinition();
+            sd.homepage = format!"https://github.com/%s/%s"(m[GithubIndex.Owner],
+                    m[GithubIndex.Project]);
+            sd.name = m[GithubIndex.Project];
+            sd.versionIdentifier = m[GithubIndex.Version];
+            return Nullable!(SourceDefinition, SourceDefinition.init)(sd);
+        }
 
         return ret;
     }
