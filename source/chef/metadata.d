ac23a4a026bebc210d867429be34f822378def3b
Ikey Doherty <ikey@serpentos.com>
2022-06-14 19:59:43 +0100

    chef/metadata: Stash some useful regexes
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/chef/metadata.d
+++ source/chef/metadata.d
@@ -12,7 +12,24 @@
 module chef.metadata;
 
 import moss.format.source;
+import std.regex;
 
+/**
+ * Standard/basic version detection
+ */
+auto reBasicVersion = ctRegex!(r"^([a-zA-Z0-9-]+)-([a-zA-Z0-9._-]+)\.(zip|tar|sh|bin\.*)");
+
+/**
+ * Github automatically generated downloads
+ */
+auto reGithubAutomatic = ctRegex!(
+        r"\w+\:\/\/github\.com\/([A-Za-z0-9-_]+)\/([A-Za-z0-9-_]+)\/archive\/refs\/tags\/([A-Za-z0-9.-_]+)\.(tar|zip)");
+
+/**
+ * Manually uploaded files on GitHub
+ */
+auto reGithubManual = ctRegex!(
+        r"\w+\:\/\/github\.com\/([A-Za-z0-9-_]+)\/([A-Za-z0-9-_]+)\/releases\/download\/([A-Za-z0-9-_.]+)\/.*");
 /**
  * Metadata provides the methods and members required to manipulate
  * and detect Metadata for a package.
@@ -21,4 +38,15 @@ public struct Metadata
 {
     SourceDefinition source;
     UpstreamDefinition[] upstreams;
+
+    /**
+     * If our source is empty, try to update it from the given URI
+     */
+    void updateSource(in string uri)
+    {
+        if (source != SourceDefinition.init)
+        {
+            return;
+        }
+    }
 }
