5b6f1dfd1bb47a3c3f3394f1107bf34d84046ac7
Ikey Doherty <ikey@serpentos.com>
2022-06-15 20:27:06 +0100

    drafter/build/autotools: Stash an unholy regex
    This will let us find PKG_CHECK_MODULES calls and use group 2 to
    discover the thing being queried. It's ugly but sure fuck it, it'll
    get the job done.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/drafter/build/autotools.d
+++ source/drafter/build/autotools.d
@@ -9,6 +9,15 @@
 
 module drafter.build.autotools;
 
+import std.regex;
+
+/**
+ * Ugly as all living fuck but lets us discover pkgconfig dependencies
+ * in configure.ac files using group index 2.
+ */
+static immutable reConfigurePkgconfig = ctRegex!(
+        r"PKG_CHECK_MODULES\s?\(\s?\[([A-Za-z_]+)\s?\]\s?,\s?\[\s?(\s?[A-Za-z0-9\-_+]+)\s?]");
+
 public struct AutotoolsBuild
 {
     string setup()
