8526fcfe0b018626e9d5debdf5dbe1829cb4b676
Ikey Doherty <ikey@serpentos.com>
2022-06-16 22:58:06 +0100

    license/engine: Preload all SPDX license data
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/drafter/license/engine.d
+++ source/drafter/license/engine.d
@@ -11,6 +11,32 @@
 
 module drafter.license.engine;
 
+import std.exception : enforce;
+import std.file : dirEntries, SpanMode, exists, DirEntry;
+import std.path : baseName;
+import std.algorithm : map, filter, each;
+import std.uni : isWhite, toLower;
+import std.array : array;
+import std.experimental.logger;
+import std.parallelism : taskPool;
+import drafter.license : License;
+import std.mmfile;
+import std.container.rbtree;
+
+/**
+ * Load SPDX license data from disk
+ */
+static private License* loadLicense(DirEntry entry)
+{
+    scope inp = new MmFile(entry.name);
+    auto text = cast(string)((cast(ubyte[]) inp[0 .. $]));
+    auto filteredText = text.filter!((c) => !c.isWhite)
+        .map!((c) => c.toLower);
+    auto bn = entry.name.baseName;
+    auto licenseName = bn[0 .. $ - 4];
+    return new License(licenseName, cast(string) filteredText.array, false);
+}
+
 /**
  * Licensing engine performs preloading and computation of
  * license specifics.
@@ -18,4 +44,22 @@ module drafter.license.engine;
 public final class Engine
 {
 
+    /**
+     * Preload all of our licenses
+     */
+    void loadFromDirectory(in string directory)
+    {
+        enforce(directory.exists);
+        trace("Preloading license data");
+
+        auto entries = dirEntries(directory, "*.txt", SpanMode.shallow, false).array;
+        auto data = taskPool.amap!loadLicense(entries);
+        licenses = new LicenseTree();
+        licenses.insert(data);
+    }
+
+private:
+
+    alias LicenseTree = RedBlackTree!(License*, "a.identifier < b.identifier", false);
+    LicenseTree licenses;
 }
