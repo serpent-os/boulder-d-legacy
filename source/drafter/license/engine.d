a725edccd3cdba4e7e126394079e0fd78aa5cc62
Ikey Doherty <ikey@serpentos.com>
2022-06-16 22:26:24 +0100

    license: Add skeleton for the Engine
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/drafter/license/engine.d
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: Zlib */
+
+/**
+ * Drafter - License engine
+ *
+ * Preloading and comparison of licenses
+ *
+ * Authors: © 2020-2022 Serpent OS Developers
+ * License: ZLib
+ */
+
+module drafter.license.engine;
+
+/**
+ * Licensing engine performs preloading and computation of
+ * license specifics.
+ */
+public final class Engine
+{
+
+}
