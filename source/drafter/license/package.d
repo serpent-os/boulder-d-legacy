161ae3c515fd95f5f99205253d528ccc05baff8d
Ikey Doherty <ikey@serpentos.com>
2022-06-16 22:12:34 +0100

    drafter: Start a new license submodule
    Using the SPDX license data we'll provide a way to automatically detect
    licenses present in source trees for compliance and ease of life for
    developers.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/drafter/license/package.d
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: Zlib */
+
+/**
+ * Drafter - License management
+ *
+ * Analyse / compare licenses
+ *
+ * Authors: © 2020-2022 Serpent OS Developers
+ * License: ZLib
+ */
+
+module drafter.license;
