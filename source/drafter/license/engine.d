a2d617e7f0ec1b76b739f1d528a638032d52eb9d
Ikey Doherty <ikey@serpentos.com>
2022-06-22 15:27:31 +0100

    license/engine: Support the REUSE spec for KDE
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/drafter/license/engine.d
+++ source/drafter/license/engine.d
@@ -15,7 +15,7 @@ import drafter : Drafter;
 import drafter.license : License;
 import moss.deps.analysis;
 import std.algorithm : each, filter, map, mean, multiSort;
-import std.array : array, split;
+import std.array : array, split, join;
 import std.container.rbtree;
 import std.conv : to;
 import std.exception : enforce;
@@ -23,9 +23,9 @@ import std.experimental.logger;
 import std.file : dirEntries, DirEntry, exists, SpanMode;
 import std.mmfile;
 import std.parallelism : taskPool;
-import std.path : baseName;
+import std.path : baseName, dirName;
 import std.range : take, chunks, zip, lockstep, Take, empty, front, enumerate;
-import std.string : startsWith, toLower;
+import std.string : startsWith, toLower, endsWith;
 import std.uni : byCodePoint, isAlphaNum;
 
 /**
@@ -76,28 +76,50 @@ static private AnalysisReturn scanLicenseFile(scope Analyser an, ref FileInfo fi
 
     auto dr = an.userdata!Drafter;
     auto bn = fi.path.baseName.toLower;
-    if (bn.startsWith("copying") || bn.startsWith("license") || bn.startsWith("licence"))
+    /**
+     * copying\.?.*
+     * license\.?.*
+     */
+    static bool isTraditionalLicense(in string licensePath)
     {
-        string text = sanitizeLicense(fi.fullPath);
-        if (text.length < MinimumScanThreshold)
-        {
-            return AnalysisReturn.NextHandler;
-        }
-        auto detectedLicenses = dr.licenseEngine.checkLicenses(bn, text)
-            .filter!((l) => l.confidence >= ConfidenceLimit).array;
-        /* HAX: Ensure "or-later" is before "only" (compliance) */
-        detectedLicenses.multiSort!("a.confidence > b.confidence", "a.id > b.id");
-        if (detectedLicenses.empty)
-        {
-            warningf("Unknown license for: %s", fi.path);
-            return AnalysisReturn.NextHandler;
-        }
-        auto top = detectedLicenses.front;
-        tracef("[LICENSE] %s: %s (Confidence: %.2f)", fi.path, top.id, top.confidence);
-        dr.insertLicense(top.id);
-        return AnalysisReturn.IncludeFile;
+        immutable auto bn = licensePath.baseName.toLower;
+        return bn.startsWith("copying") || bn.startsWith("license") || bn.startsWith("licence");
     }
-    return AnalysisReturn.NextHandler;
+
+    /**
+     * REUSE compliance?
+     */
+    static bool isREUSE(in string licensePath)
+    {
+        immutable auto bn = licensePath.baseName.toLower;
+        auto dd = licensePath.dirName.baseName.toLower;
+        return dd == "licenses" && bn.endsWith(".txt");
+    }
+
+    if (!isTraditionalLicense(fi.fullPath) && !isREUSE(fi.fullPath))
+    {
+        return AnalysisReturn.NextHandler;
+    }
+
+    string text = sanitizeLicense(fi.fullPath);
+    if (text.length < MinimumScanThreshold)
+    {
+        return AnalysisReturn.NextHandler;
+    }
+
+    auto detectedLicenses = dr.licenseEngine.checkLicenses(bn, text)
+        .filter!((l) => l.confidence >= ConfidenceLimit).array;
+    /* HAX: Ensure "or-later" is before "only" (compliance) */
+    detectedLicenses.multiSort!("a.confidence > b.confidence", "a.id > b.id");
+    if (detectedLicenses.empty)
+    {
+        warningf("Unknown license for: %s", fi.path);
+        return AnalysisReturn.NextHandler;
+    }
+    auto top = detectedLicenses.front;
+    tracef("[LICENSE] %s: %s (Confidence: %.2f)", fi.path, top.id, top.confidence);
+    dr.insertLicense(top.id);
+    return AnalysisReturn.IncludeFile;
 }
 
 /**
@@ -169,16 +191,27 @@ public final class Engine
         /* If we hae COPYING.$SPDX, use the id up front. */
         auto ppath = path.baseName.toLower;
         auto splits = ppath.split(".").array;
+
+        /* If we have a .SUFFIX, check it */
         if (splits.length > 1)
         {
             auto nom = splits[1];
-            auto matchingLicenses = licenses.filter!((m) => m.identifier.toLower == nom
-                    || m.identifier.toLower == ppath)
+            auto matchingLicenses = licenses.filter!((m) => m.identifier.toLower == nom)
                 .map!((l) => LicenseResult(l.identifier, 1.0));
             if (!matchingLicenses.empty)
             {
-                return [matchingLicenses.front];
+                return matchingLicenses.array;
             }
+        }
+
+        /* Do we have a valid match for the first field ($SPDX.txt) ? (i.e. REUSE) */
+        const auto baseToSuffix = join(splits.length > 1 ? splits[0 .. $ - 1] : [
+                splits[0]
+                ], ".");
+        auto identicalIDs = licenses.filter!((m) => m.identifier.toLower == baseToSuffix);
+        if (!identicalIDs.empty)
+        {
+            return identicalIDs.map!((m) => LicenseResult(m.identifier, 1.0)).array;
         }
 
         /* Map to eliminate dual context limitation in LDC */
