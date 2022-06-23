93e8ae229640fe3e555df843268ecc1d038869d5
Joey Riches <josephriches@gmail.com>
2022-06-23 14:03:27 +0100

    new: Initial python handler
    Initial python handler that will handle setuptools and pep517 projects.

---
+++ source/drafter/build/python.d
@@ -0,0 +1,93 @@
+/* SPDX-License-Identifier: Zlib */
+
+/**
+ * Drafter - Python integration
+ *
+ * Authors: © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module drafter.build.python;
+import moss.deps.analysis;
+import drafter : Drafter;
+import drafter.build : BuildType;
+import std.path : baseName;
+
+/**
+ * Discover python projects
+ */
+static private AnalysisReturn acceptPython(scope Analyser an, ref FileInfo inpath)
+{
+    Drafter c = an.userdata!Drafter;
+    auto bn = inpath.path.baseName;
+    import std.string : count;
+
+    switch (bn)
+    {
+    case "pyproject.toml":
+    case "setup.cfg":
+        c.incrementBuildConfidence(BuildType.PythonPEP517, 100);
+        return AnalysisReturn.IncludeFile;
+    case "setup.py":
+        c.incrementBuildConfidence(BuildType.PythonSetuptools, 100);
+        return AnalysisReturn.IncludeFile;
+    default:
+        return AnalysisReturn.NextHandler;
+    }
+}
+
+/**
+ * Handler for python projects
+ */
+public static AnalysisChain pythonChain = AnalysisChain("python", [&acceptPython], 20);
+
+/**
+ * Python PEP517 Build (pyproject.toml/setup.cfg)
+ */
+public struct Pythonpep517Build
+{
+    string setup()
+    {
+        return null;
+    }
+    string build()
+    {
+        return "%pyproject_build";
+    }
+
+    string install()
+    {
+        return "%pyproject_install";
+    }
+
+    string check()
+    {
+        return null;
+    }
+}
+
+/**
+ * Python Setuptools Build (setup.py)
+ */
+public struct PythonsetuptoolsBuild
+{
+    string setup()
+    {
+        return null;
+    }
+
+    string build()
+    {
+        return "%python_build";
+    }
+
+    string install()
+    {
+        return "%python_install";
+    }
+
+    string check()
+    {
+        return null;
+    }
+}
