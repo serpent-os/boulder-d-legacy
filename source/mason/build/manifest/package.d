d1ba951d6e9e381064644e815d6ccfd2f8c4d606
Ikey Doherty <ikey@serpentos.com>
2022-10-03 14:29:16 +0100

    manifest: Absorb metadata generation for deduplication
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/mason/build/manifest/package.d
+++ source/mason/build/manifest/package.d
@@ -16,10 +16,14 @@
 module mason.build.manifest;
 
 public import moss.deps.analysis;
+public import moss.format.binary.payload.layout;
+public import moss.format.binary.payload.meta;
 public import moss.format.source.package_definition;
 public import moss.format.source.source_definition;
-
-public import moss.format.binary.payload.layout;
+import std.algorithm : each, filter, map, sort, uniq;
+import std.array : array;
+import std.experimental.logger : info;
+import std.string : format;
 
 /**
  * Resulting Package is only buildable once it contains
@@ -45,6 +49,65 @@ public struct Package
         return "%s-%s-%d-%d-%s.stone".format(pd.name, source.versionIdentifier,
                 source.release, buildRelease, plat.name);
     }
+}
+
+/**
+     * Generate metadata payload
+     */
+public MetaPayload generateMetadata(scope AnalysisBucket bucket,
+        scope Package* pkg, bool shouldLog = true) @trusted
+{
+    auto met = new MetaPayload();
+    met.addRecord(RecordType.String, RecordTag.Name, pkg.pd.name);
+    met.addRecord(RecordType.String, RecordTag.Version, pkg.source.versionIdentifier);
+    met.addRecord(RecordType.Uint64, RecordTag.Release, pkg.source.release);
+    met.addRecord(RecordType.Uint64, RecordTag.BuildRelease, pkg.buildRelease);
+    met.addRecord(RecordType.String, RecordTag.Summary, pkg.pd.summary);
+    met.addRecord(RecordType.String, RecordTag.Description, pkg.pd.description);
+    met.addRecord(RecordType.String, RecordTag.Homepage, pkg.source.homepage);
+    met.addRecord(RecordType.String, RecordTag.SourceID, pkg.source.name);
+
+    /* TODO: Be more flexible encoding architecture. */
+    import moss.core.platform : platform;
+
+    auto plat = platform();
+    met.addRecord(RecordType.String, RecordTag.Architecture, plat.name);
+
+    pkg.source.license.sort();
+    pkg.source.license.uniq.each!((l) => met.addRecord(RecordType.String, RecordTag.License, l));
+
+    auto providers = bucket.providers();
+    auto specifiedDeps = pkg.pd.runtimeDependencies.map!((const n) => fromString!Dependency(n));
+    auto discoveredDeps = bucket.dependencies();
+    auto dependenciesFull = specifiedDeps.array() ~ discoveredDeps.array();
+    dependenciesFull.sort();
+    auto dependencies = dependenciesFull.uniq();
+
+    if (!providers.empty)
+    {
+        foreach (prov; providers)
+        {
+            if (shouldLog)
+            {
+                info(format!"[%s] provides %s"(pkg.pd.name, prov));
+            }
+            met.addRecord(RecordType.Provider, RecordTag.Provides, prov);
+        }
+    }
+    if (!dependencies.empty)
+    {
+        foreach (dep; dependencies)
+        {
+            if (shouldLog)
+            {
+                info(format!"[%s] depends on %s"(pkg.pd.name, dep));
+            }
+            met.addRecord(RecordType.Dependency, RecordTag.Depends, dep);
+        }
+    }
+
+    return met;
+
 }
 
 /**
