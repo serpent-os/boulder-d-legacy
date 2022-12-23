bbb80ed10da815b9fb2f71b36b42a8534ef368f1
Ikey Doherty <ikey@serpentos.com>
2022-12-23 02:30:46 +0000

    mason/build/builder: Detect dependencies for .pc files
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/mason/build/builder.d
+++ source/mason/build/builder.d
@@ -323,6 +323,30 @@ private:
         auto providerName = fileInfo.path.baseName()[0 .. $ - 3];
         auto prov = Provider(providerName, ProviderType.PkgconfigName);
         analyser.bucket(fileInfo).addProvider(prov);
+
+        string[] cmd = [
+            "/usr/bin/pkg-config", "--print-requires",
+            "--print-requires-private", "--silence-errors", fileInfo.fullPath
+        ];
+        import std.process : execute;
+        import std.string : split, splitLines;
+        import std.algorithm : map;
+
+        string[string] env;
+        env["LC_ALL"] = "C";
+        auto ret = execute(cmd, env);
+
+        if (ret.status != 0)
+        {
+            error(format!"Failed to run pkg-config: %s"(ret.output));
+            return AnalysisReturn.NextFunction;
+        }
+        auto deps = ret.output.splitLines().map!((l) => l.split[0]);
+        foreach (d; deps)
+        {
+            auto dep = Dependency(d, DependencyType.PkgconfigName);
+            analyser.bucket(fileInfo).addDependency(dep);
+        }
         return AnalysisReturn.NextHandler;
     }
 
