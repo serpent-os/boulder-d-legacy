d94f9fad42f1912feb37279777d2ac7aaeda5f96
Joey Riches <josephriches@gmail.com>
2023-03-24 18:07:39 +0000

    boulder/cli: Add chroot subcommand
    This allows the user to chroot into a recipe target using moss-container
    by initalizing a controller and running controller.chroot(recipe).

---
+++ source/boulder/cli/chroot_command.d
@@ -0,0 +1,106 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * boulder.cli.chroot_command
+ *
+ * Helper to chroot into a recipe build location with moss-container
+ *
+ * Authors: Copyright © 2020-2023 Serpent OS Developers
+ * License: Zlib
+ */
+
+module boulder.cli.chroot_command;
+
+import boulder.cli : BoulderCLI;
+import boulder.controller;
+import core.sys.posix.unistd : geteuid;
+import mason.build.context : buildContext;
+import mason.build.util : executeCommand;
+import moss.core : ExitStatus;
+import std.experimental.logger;
+import std.file : exists, thisExePath;
+import std.format : format;
+import std.path : absolutePath, buildNormalizedPath, dirName;
+public import moss.core.cli;
+
+/**
+ * The ChrootCommand is responsible for handling requests to chroot into
+ * a stone.yml's build location
+ */
+@CommandName("chroot") @CommandAlias("cr")
+@CommandHelp("Chroot into a recipe's build location using moss-container.")
+@CommandUsage("[recipe]")
+public struct ChrootCommand
+{
+    /** Extend BaseCommand with ChrootCommand specific functionality */
+    BaseCommand pt;
+    alias pt this;
+
+    /**
+     * Main entry point into the ChrootCommand where we expect a valid recipe
+     * (stone.yml) file
+     *
+     * Once all validation is passed, we chroot into the recipe's build location
+     * using `moss-container`.
+     *
+     * Params:
+     *      argv = arguments passed to command line
+     * Returns: ExitStatus.Success on success, ExitStatus.Failure on failure.
+     */
+    @CommandEntry() int run(ref string[] argv)
+    {
+        immutable useDebug = this.findAncestor!BoulderCLI.debugMode;
+        globalLogLevel = useDebug ? LogLevel.trace : LogLevel.info;
+
+        immutable profile = this.findAncestor!BoulderCLI.profile;
+        immutable configDir = this.findAncestor!BoulderCLI.configDir;
+
+        if (argv.length > 1)
+        {
+            warning("Unexpected number of arguments declared. For help, run boulder chroot -h");
+            return ExitStatus.Failure;
+        }
+
+        /* Ensure root permissions */
+        if (geteuid() != 0)
+        {
+            error("boulder must be run with root privileges.");
+            return ExitStatus.Failure;
+        }
+
+        /* Check moss-container exists */
+        immutable binDir = thisExePath.dirName;
+        immutable containerBinary = binDir.buildNormalizedPath("moss-container").absolutePath;
+        if (!containerBinary.exists)
+        {
+            error(format!"Cannot find `moss-container` at: %s"(containerBinary));
+            return ExitStatus.Failure;
+        }
+
+        /* Use stone.yml in current dir if no args passed, otherwise the recipe is the first arg */
+        immutable recipe = argv.length > 0 ? argv[0] : "stone.yml";
+        if (!recipe.exists)
+        {
+            error(format!"Cannot find recipe to chroot into build location: %s"(recipe));
+            return ExitStatus.Failure;
+        }
+
+        /* Dummy vars to create a controller */
+        immutable outputDirectory = ".";
+        immutable unconfined = false;
+        immutable compilerCache = false;
+        immutable architecture = "native";
+        /* Create the controller */
+        auto controller = new Controller(outputDirectory, architecture,
+                !unconfined, profile, compilerCache, configDir);
+
+        /* Chroot into the recipe */
+        controller.chroot(recipe);
+
+        return ExitStatus.Success;
+    }
+}
