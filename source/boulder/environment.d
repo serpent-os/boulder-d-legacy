1944143b9ec547c1bf726630d80d0498714d955a
Cory Forsstrom <cforsstrom18@gmail.com>
2023-04-20 20:02:33 -0700

    Compile version & git hash and use in version command

---
+++ source/boulder/environment.d
@@ -0,0 +1,37 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * boulder.environment
+ *
+ * Environment variables
+ *
+ * Authors: Copyright © 2020-2023 Serpent OS Developers
+ * License: Zlib
+ */
+
+module boulder.environment;
+
+import std.format : format;
+import std.range;
+
+/** Boulder version */
+const VERSION = "@VERSION@";
+/** Git version */
+const GIT_VERSION = "@GIT_VERSION@";
+
+/** 
+ * Returns the full formatted version string including 
+ * git hash (if compiled from the repo)
+ *
+ * e.g. 0.1.0 (1d2f6b389)
+ */
+string fullVersion()
+{
+    immutable git = GIT_VERSION.empty ? "" : format!" (%s)"(GIT_VERSION);
+
+    return format!"%s%s"(VERSION, git);
+}
